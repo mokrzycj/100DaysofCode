35f4ed26a7c21b70bad7a62588ea240986eb319b
mokrzycj <mokrzycj@gmail.com>
2023-10-10 22:56:23 +0200

    day-003

---
+++ Day-002/tip-calculator.py
@@ -0,0 +1,18 @@
+print('Welcome to the Tip Calculator')
+
+totalAmount = input("what was the total amount? ")
+peopleNumber = input("How many people to split the bill? ")
+tipAmount = input('What percentage tip would you like to give? 10%, 12% or 15% ')
+
+totalWithTip=int(totalAmount)*(1+int(tipAmount)/100)
+perPerson = round(int(totalWithTip)/int(peopleNumber),2)
+
+print("Each person should pay: " + str(perPerson))
+
+# PEMDASLR
+# ()
+# **
+# * /
+# + -
+
+# 8//3 = integer (1)
